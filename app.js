eb62003c3db898d39c2991c665b1219ba1663adf
EnriqueHM <enriqhz@gmail.com>
2021-07-07 20:40:54 -0500

    se agrega funcion con JS

---
+++ app.js
@@ -0,0 +1,34 @@
+const $forms = document.querySelectorAll(".signup-form")
+//console.log($forms);
+/*
+const sendEmail = (miVariable) => {
+    miVariable.preventDefault();
+    console.log(miVariable);
+}*/
+
+function sendEmail(params){
+    params.preventDefault();
+    console.log(params);
+    const email = params.target.querySelector("input").value
+    console.log(email);
+    getTemplate()
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log(error, "error al obtener el template");
+    })
+}
+
+const getTemplate = () => {
+    return fetch("./template.html")
+    .then((response) => response.text())    
+} 
+
+for(let i = 0; i < $forms.length; i++){
+   //console.log($forms[i]); 
+   $forms[i].addEventListener("submit", sendEmail)
+}
+
+
+/* revisar archivos dribble */
